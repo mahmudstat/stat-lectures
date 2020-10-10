dbbe81c0e1e8c205a3ece083a02d14d129831157
mahmudstat <almahmud.sbi@gmail.com>
2020-10-10 22:34:06 +0600

    Add disperion functions, like CR, CV, CMD, CQD

---
+++ r/dispersion.R
@@ -0,0 +1,18 @@
+# Mean Deviation
+# About Mean
+mdm <- function(x) sum(abs(x-mean(x)))/length(x)
+
+# About Median 
+mdmed <- function(x) sum(abs(x-median(x)))/length(x)
+
+# Coefficient of Range
+cr <- function(x) (range(x)[2]-range(x)[1])/(range(x)[2]+range(x)[1])*100
+
+# Coefficient of Mean Deviation 
+# About Mean
+cmdm <- function(x) mdm(x)/mean(x)*100
+
+# Coefficient of Variance
+
+cv <- function(x) sd(x)/mean(x)*100
+
